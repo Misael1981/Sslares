ec33ea21728e89f9c6bad97e6c6c6e30c431ee9c
Misael Borges <ml_bbd@hotmail.com>
2024-04-02 17:05:16 -0300

    atualização

--- add.js
+++ add.js
@@ -275,7 +275,21 @@ function calcularTotalEspacadores() {
 */
 
 
-// Sobre nós 
+// Modal
+
+const openModalButton = document.querySelector("#open-modal"); // Abrir Modal
+const closeModalButton = document.querySelector("#close-modal"); // Fechar Modal 
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
 
 
 
--- main.js
+++ main.js
@@ -93,3 +93,19 @@ abstract.addEventListener('mouseleave', function() {
     // Limpa o conteúdo do parágrafo
     paragrafoUm.innerHTML = "";
 });
+
+// Modal
+
+const openModalButton = document.querySelector("#open-modal"); // Abrir Modal
+const closeModalButton = document.querySelector("#close-modal"); // Fechar Modal 
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
