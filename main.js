e41f8079a5e33fb7cbb4dad97031ed98efce4668
Misael Borges <ml_bbd@hotmail.com>
2024-04-20 23:28:43 -0300

    Começando a finalizar

--- main.js
+++ main.js
@@ -29,48 +29,7 @@ var swiper = new Swiper(".home-slider", {
     },
 });
 
-var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop:true,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
-    breakpoints:{
-        0:{
-            slidesPerView:1,
-        },
-        600:{
-            slidesPerView:2,
-        },
-    },
-});
 
-var swiper = new Swiper(".blogs-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop:true,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints:{
-        0:{
-            slidesPerView:1,
-        },
-        768:{
-            slidesPerView:2,
-        },
-        991:{
-            slidesPerView:3,
-        },
-    },
-});
 
 // Calculadora 
 
